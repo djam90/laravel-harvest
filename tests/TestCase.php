7dbb066a2e0237e90299a20ee12d948f410f57d4
djam90 <hello@danieltwigg.co.uk>
2018-08-01 14:04:39 +0100

    Support for unit testing with PHPUnit and Orchestra

---
+++ tests/TestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Djam90\Harvest\Test;
+
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+class TestCase extends OrchestraTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+    }
+}
