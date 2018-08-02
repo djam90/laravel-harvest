9e65c8367858675a55c4f42ea30e4c329d217fb4
djam90 <hello@danieltwigg.co.uk>
2018-08-02 12:20:11 +0100

    Add support for project to get latest time entry

--- src/Models/Project.php
+++ src/Models/Project.php
@@ -2,7 +2,25 @@
 
 namespace Djam90\Harvest\Models;
 
+use Djam90\Harvest\HarvestService;
+
 class Project extends Base
 {
+    public function getLatestTimeEntry()
+    {
+        $harvestService = app('harvest');
 
+        return $harvestService->timeEntry->get(
+            null,
+            null,
+            $this->id,
+            null,
+            null,
+            null,
+            null,
+            null,
+            1,
+            1
+        )->time_entries->first();
+    }
 }
